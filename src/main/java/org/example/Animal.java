256692db32b2cadcd047c18da10a38a840636b62
JoseaSosa <josea_1998@htomail.com>
2023-02-16 11:36:34 -0500

    se crea proyeccto zoologico

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -2,11 +2,49 @@ package org.example;
 
 public class Animal {
 
-    String nombre;
-    String genero;
-    double peso=1;
-    Animal pareja;
+    private String nombre;
+    private String genero;
+    private double peso=1;
+    private Animal pareja;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public Animal getPareja() {
+        return pareja;
+    }
+
+    public void setPareja(Animal pareja) {
+        this.pareja = pareja;
+    }
+
+    public Animal(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Animal() {
+    }
 
     public Animal(String nombre, double peso) {
         this.nombre = nombre;
--- src/main/java/org/example/Familia.java
+++ src/main/java/org/example/Familia.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Familia {
 
-    Animal papa="Dinosaurin";
-    Animal mama="Tiburosin";
+    Animal papa;
+    Animal mama;
     Animal hijo;
 
     public Familia(Animal papa, Animal mama) {
@@ -11,29 +11,29 @@ public class Familia {
         this.mama = mama;
     }
 
-    public  imprimirFamilia(){
+    public void imprimirFamilia(){
 
         //Este metodo muestra los nombres de los integrantes de la familia
         System.out.println(
-                "El papá es: "+this.papa.nombre+
-                        "\nLa mamá es: "+this.mama.nombre+
-                            "\nEl hijo es: "+this.hijo.nombre
+                "El papá es: "+this.papa.getNombre()+
+                        "\nLa mamá es: "+this.mama.getNombre()+
+                        "\nEl hijo es: "+this.hijo.getNombre()
         );
 
     }
 
     public void tenerHijo(String nombre){
 
-        if(this.papa.pareja!=null){
+        if(this.papa.getPareja()!=null){
 
             Animal hijo= new Animal();
-            hijo.nombre=nombre;
-            hijo.peso=1;
+            hijo.setNombre(nombre);
+            hijo.setPeso(1);
             double random = Math.random();
             if(random<0.5){
-                hijo.genero="F";
+                hijo.setGenero("F");
             }else{
-                hijo.genero="M";
+                hijo.setGenero("M");
             }
             this.hijo=hijo;
 
